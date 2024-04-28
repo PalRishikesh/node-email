ba99efb9deae760fcf6f8e7132fe27226b4ec1be
PalRishikesh <palrishikesh007@gmail.com>
2024-04-28 14:46:56 +0530

    validation message structure changes doen

--- src/helpers/validator.js
+++ src/helpers/validator.js
@@ -1,40 +1,41 @@
 const { body, validationResult } = require("express-validator");
- 
-const  contactFormValidationRules = () => {
-    return [
-        body("name").notEmpty(),
-        body("email").isEmail(),
-        body("contact").notEmpty(),
-        body("industry").notEmpty(),
-        body("message").notEmpty(),
-    
-    ];
-}
 
-const  careerFormValidationRules = () => {
-    return [
-        body("name").notEmpty(),
-        body("email").isEmail(),
-        body("contact").notEmpty(),
-        body("message").notEmpty(),
-    
-    ];
-}
+const contactFormValidationRules = () => {
+  return [
+    body("name").notEmpty(),
+    body("email").isEmail(),
+    body("contact").notEmpty(),
+    body("industry").notEmpty(),
+    body("message").notEmpty(),
+  ];
+};
 
-const isContactFormValidation = (req, res, next)=>{
-    const error = validationResult(req)
-    if(error.isEmpty()){
-        return next();
-    }
-    const extractedErrors = []
-    error.array().map(err => extractedErrors.push({ [err.path]: err.msg }) )
-    return res.status(422).json({
-        errors: extractedErrors,
-    })
-}
+const careerFormValidationRules = () => {
+  return [
+    body("name").notEmpty(),
+    body("email").isEmail(),
+    body("contact").notEmpty(),
+    body("message").notEmpty(),
+  ];
+};
+
+const isContactFormValidation = (req, res, next) => {
+  const error = validationResult(req);
+  if (error.isEmpty()) {
+    return next();
+  }
+
+  const extractedErrors = [];
+  error.array().map((err) => extractedErrors.push({ [err.path]: err.msg }));
+  return res.status(422).json({
+    status: 0,
+    message: "Please filled the form value correct",
+    errors: extractedErrors,
+  });
+};
 
 module.exports = {
-    contactFormValidationRules,
-    careerFormValidationRules,
-    isContactFormValidation
-}
+  contactFormValidationRules,
+  careerFormValidationRules,
+  isContactFormValidation,
+};
