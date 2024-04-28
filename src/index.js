47c020c5820704a29be9de1ea774232b130666e1
PalRishikesh <palrishikesh007@gmail.com>
2024-04-28 16:41:59 +0530

    node engines

--- src/index.js
+++ src/index.js
@@ -2,14 +2,19 @@ const express = require("express");
 const cors = require("cors");
 const helment = require("helmet");
 const emailRouter = require("./routers/emailRouter");
+require("dotenv").config();
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(helment())
 
-const PORT = 3000;
-
+const PORT = process.env.PORT || 5000;
+app.get("/",(req,res)=>{
+    return res.send({
+        message:"Home page is here"
+    })
+})
 app.use("/api/v1/",emailRouter)
 
 
